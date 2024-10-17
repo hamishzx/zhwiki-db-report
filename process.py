e76ce95ccb7ff004e44efd22e2fb16eb5e945c65
CHAU SHING SHING HAMISH <hamishz@toolforge.org>
2024-10-17 11:26:26 +0800

    add file to save zhwiki page

---
+++ process.py
@@ -0,0 +1,20 @@
+# ！/usr/bin/env python
+# -*-coding:Utf-8 -*-
+# Time: 17 Oct 2024 11:22
+# Name: edit.py
+# Author: CHAU SHING SHING HAMISH
+
+import pywikibot
+
+raw_txt = 'report1.txt'
+page_text = '* 生成時間：~~~~~\n'
+with open(raw_txt, 'r') as infile:
+    for line in infile:
+        line = line.strip()
+        page_text += f'# [[:File:{line}]]\n'
+
+site = pywikibot.Site('zh', 'wikipedia')
+report_page = pywikibot.Page(site, 'Wikipedia:資料庫報告/檔案描述頁')
+
+report_page.text = page_text
+report_page.save('[[Wikipedia:机器人/申请/Hamish-bot/8|T8]]：生成本地存在檔案描述的維基共享資源文件列表')
